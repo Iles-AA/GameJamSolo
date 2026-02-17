746a5d52d3ea41b47f9c6f9bc568ac9dea9a4d00
Iles-AA <aitabbailes@gmail.com>
2026-02-17 15:50:09 +0100

    Sprite , physics , collision added , beta stage created

--- My project/Assets/Script/TopDownMovement.cs
+++ My project/Assets/Script/TopDownMovement.cs
@@ -6,8 +6,12 @@ public class TopDownMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private Animator animator;
 
-    private Rigidbody2D physics;
+    public SpriteRenderer spriteRenderer;
+
+
+    public Rigidbody2D physics;
     private Vector2 direction;
 
     private void Awake()
@@ -15,17 +19,28 @@ public class TopDownMovement : MonoBehaviour
         physics = GetComponent<Rigidbody2D>();
     }
 
-
-public void OnMove(InputValue value)
-{
-    direction = value.Get<Vector2>();
-}
-
     private void OnEnable() => moveAction.action.Enable();
     private void OnDisable() => moveAction.action.Disable();
 
+    private void Update()
+    {
+        float magnitude = moveAction.action.ReadValue<Vector2>().sqrMagnitude;
+        direction = moveAction.action.ReadValue<Vector2>();
+
+        animator.SetFloat("Speed", magnitude);
+
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
     private void FixedUpdate()
     {
-        physics.MovePosition(physics.position + direction.normalized * moveSpeed * Time.fixedDeltaTime);
+        physics.MovePosition(physics.position + direction.normalized * moveSpeed * Time.fixedDeltaTime);    
     }
 }
